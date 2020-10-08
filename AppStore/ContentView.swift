3b6a72c7dba24a87569a823018340d36c582c3ba
DJDrama <putzzang@hotmail.com>
2020-10-08 10:37:53 +0900

    Bottom Tab bar + Today Page with SwiftUi

---
+++ AppStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStore
+//
+//  Created by jb on 2020/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AppStore/GamesPage.swift
@@ -0,0 +1,22 @@
+//
+//  GamesPage.swift
+//  AppStore
+//
+//  Created by jb on 2020/10/08.
+//
+
+import Foundation
+import SwiftUI
+
+struct GamesView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct GamesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
